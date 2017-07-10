bf11f51a889fa204bfb9c37704c7e6fbc30cf7a8
Hernan Tejada <hernan.tejada@gmail.com>
2017-07-10 00:53:21 -0400

    firebase clean up files

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,6 +12,7 @@ firebase.initializeApp(config);
 var playersRef = firebase.database();
 var playersdbRef = playersRef.ref().child("players");
 var chatdbRef = playersRef.ref().child("chat");
+var playersconnectedRef = playersRef.ref(".info/connected");
 var gameChoices = ["Rock", "Paper", "Scissors"];
 var p1name = "";
 var p2name = "";
@@ -21,62 +22,160 @@ var p1wins = 0;
 var p1losses = 0;
 var p2wins = 0;
 var p2losses = 0;
-var gameTurns = 1;
-var choice;
-var losses;
-var name;
-var wins;
+var gameTurns = 0;
+var p1iteration = false;
+var p2iteration = false;
 
-function firstPlayer(){  
-  $("#add-player").on("click", function(event){
-    event.preventDefault();
+playersconnectedRef.on("value", function(snap) {
+  console.log(snap.val());
+});
+
+function resetGame() {  
+  playersRef.ref().remove();
+  firstPlayer();  
+};
+
+function startOver(){    
+  gameTurns = 1;
+  var p1iteration = false;
+  var p2iteration = false;
+  setTimeout(newGame, 3000);
+};
+
+function newGame() {  
+  playersRef.ref().update({      
+    turn: gameTurns
+  }); 
+  $("#player1Choice").empty();
+  $("#player2Choice").empty();
+  $("#player").empty();
+  $("#results").empty();
+  firstplayerChoice();          
+};
+
+playersRef.ref("players/1/").on("value", function(snapshot) { 
+  console.log(snapshot.val());
+  $("#player1").html(snapshot.val().name);
+  p1name = snapshot.val().name;          
+});   
+
+playersRef.ref("players/2/").on("value", function(snapshot) {
+  console.log(snapshot.val());        
+  $("#player2").html(snapshot.val().name);
+  p2name = snapshot.val().name;     
+});    
+
+playersRef.ref("players/1/").on("value", function(snapshot) {
+  console.log(snapshot.val()); 
+  if (snapshot.val().choice) {
+    $("#player1Choice").html("<span class='highLight'>" + snapshot.val().choice + "</span>");
+    p1choice = snapshot.val().choice;   
+  }  
+});
+
+playersRef.ref("players/2/").on("value", function(snapshot) { 
+  console.log(snapshot.val());   
+  if (snapshot.val().choice) {
+    $("#player2Choice").html("<span class='highLight'>" + snapshot.val().choice + "</span>");
+    p2choice = snapshot.val().choice; 
+  }    
+});
+
+playersRef.ref("players/1/").on("value", function(snapshot) {
+  console.log(snapshot.val());        
+  $("#p1wins").html(snapshot.val().wins);
+  $("#p1losses").html(snapshot.val().losses);
+  p1wins = snapshot.val().wins;    
+  p1losses = snapshot.val().losses;   
+});  
+
+playersRef.ref("players/2/").on("value", function(snapshot) {
+  console.log(snapshot.val());        
+  $("#p2wins").html(snapshot.val().wins);
+  $("#p2losses").html(snapshot.val().losses);
+  p2wins = snapshot.val().wins;     
+  p2losses = snapshot.val().losses;     
+});   
+
+function firstPlayer() {  
+  
+  $("#add-player").on("click", function(event) {
+    event.preventDefault();    
     p1name = $("#player-name").val().trim();    
-    playersdbRef.child('1').set({
-      choice: "",
+    playersdbRef.child('1').set({      
       losses: 0,
       name: p1name,
       wins: 0
     });
-    playersRef.ref("players/1/").on("value", function(snapshot) { 
-      console.log(snapshot.val().name);
-      $("#player1").html(snapshot.val().name);
-      p1name = snapshot.val().name;          
-    });         
     $("#player-name").val("");  
-    $("#add-player").off("click");
-    secondPlayer(); 
-  });    
+    $("#add-player").off("click");      
+    secondPlayer();
+  }); 
 };
 
 function secondPlayer(){
-  $("#add-player").on("click", function(event){
-    event.preventDefault();
+ 
+  $("#add-player").on("click", function(event) {
+    event.preventDefault();  
+    gameTurns++; 
     p2name = $("#player-name").val().trim();    
-    playersdbRef.child('2').set({
-      choice: "",
+    playersdbRef.child('2').set({      
       losses: 0,
       name: p2name,
       wins: 0
-    });
-    playersRef.ref("players/2/").on("value", function(snapshot) {
-      console.log(snapshot.val().name);        
-      $("#player2").html(snapshot.val().name);
-      p2name = snapshot.val().name;     
-    });    
+    });   
+    playersRef.ref().update({      
+      turn: gameTurns
+    });      
     $("#player-name").val("");  
     $("#add-player").off("click");
-    newGame();  
+    newGame();
   });  
 };
 
-function startOver(){
-  gameTurns++;
-  setTimeout(newGame, 3000);
+function firstplayerChoice() {
+
+  for(var i = 0; i < gameChoices.length; i++) {
+    $("#player1Choice").append("<li>" + gameChoices[i] + "</li>");
+  }
+  $("li").on("click", function(event) {
+    event.preventDefault();
+    gameTurns++;
+    p1choice = $(this).html();
+    playersRef.ref("players/1/").update({
+      choice: p1choice
+    });
+    playersRef.ref().update({      
+      turn: gameTurns
+    }); 
+    secondplayerChoice();
+  });  
+};
+
+function secondplayerChoice() {
+  
+  for(var j = 0; j < gameChoices.length; j++) {
+    $("#player2Choice").append("<li>" + gameChoices[j] + "</li>");
+  }
+  $("li").on("click", function(event) {
+    event.preventDefault();
+    gameTurns++;
+    p2choice = $(this).html();
+    playersRef.ref("players/2/").update({
+      choice: p2choice
+    });    
+    playersRef.ref().update({      
+      turn: gameTurns
+    });
+    evaluation(p1choice, p2choice); 
+    console.log(p1choice)
+    console.log(p2choice)
+
+  });  
 };
 
 function evaluation(x, y) {
-  var p1iteration = false;
-  var p2iteration = false;
+  
   if ((x === "Rock") || (x === "Paper") || (x === "Scissors")) {
     if ((x === "Rock") && (y === "Scissors")) {
       p1wins++;
@@ -111,7 +210,7 @@ function evaluation(x, y) {
   $("#p1wins").html(p1wins); 
   playersRef.ref("players/1/").update({
     wins: p1wins
-  });  
+  });    
 
   $("#p1losses").html(p1losses);   
   playersRef.ref("players/1/").update({
@@ -126,55 +225,19 @@ function evaluation(x, y) {
   $("#p2losses").html(p2losses);  
   playersRef.ref("players/2/").update({
     losses: p2losses
-  }); 
-
+  });     
+ 
   if (p1iteration === true) {
     $("#player").html(p1name);
     $("#results").html("Wins!");
   }
+
   if (p2iteration === true) {
     $("#player").html(p2name);
     $("#results").html("Wins!");
   }
+
   startOver();
-};
-
-function newGame(){
-  $("#player1Choice").empty();
-  $("#player2Choice").empty();
-  $("#player").empty();
-  $("#results").empty();
-
-  for(var i = 0; i < gameChoices.length; i++) {
-    $("#player1Choice").append("<li>" + gameChoices[i] + "</li>");
-  }
-  $("li").on("click", function(event) {
-    event.preventDefault();
-    p1choice = $(this).html();
-    playersRef.ref("players/1/").update({
-      choice: p1choice
-    });
-    playersRef.ref("players/1/choice").on("value", function(snapshot) {
-      $("#player1Choice").html("<span class='highLight'>" + snapshot.val() + "</span>");
-      p1choice = snapshot.val();     
-    });
-
-    for(var j = 0; j < gameChoices.length; j++) {
-      $("#player2Choice").append("<li>" + gameChoices[j] + "</li>");
-    }
-    $("li").on("click", function(event) {
-      event.preventDefault();
-      p2choice = $(this).html();
-      playersRef.ref("players/2/").update({
-        choice: p2choice
-      });
-      playersRef.ref("players/2/choice").on("value", function(snapshot) {        
-        $("#player2Choice").html("<span class='highLight'>" + snapshot.val() + "</span>");
-        p2choice = snapshot.val();     
-      });
-      evaluation(p1choice, p2choice);
-    });
-  });        
 };
 
 $(function() {
@@ -182,7 +245,10 @@ $(function() {
   $("#button").on("click", function (event) {    
     event.preventDefault();
     var messageNew = $("#message-input").val();
-    chatdbRef.push({name:p1name, message:messageNew});
+    chatdbRef.push({
+      name: p1name, 
+      message: messageNew
+    });
     $("#message-input").val("");
   });
 
@@ -194,7 +260,7 @@ $(function() {
   });
 });
 
-firstPlayer();
+resetGame();
 
 
 
